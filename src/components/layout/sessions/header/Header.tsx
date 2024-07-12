577123a7aaf50fd2d93a7d85c94b195e9546829a
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-12 15:22:24 -0300

    feat: add favicon e header layout

--- src/components/layout/sessions/header/Header.tsx
+++ src/components/layout/sessions/header/Header.tsx
@@ -1,6 +1,29 @@
-
+import { Link } from 'react-router-dom';
+import Logo from '../../../../assets/logo-web-motors.svg';
+import { CircleUserRound, LogInIcon } from 'lucide-react';
 export const Header = () => {
+  const signed = true;
+  const loading = false;
+
   return (
-    <header>Header</header>
-  )
-}
+    <header className="w-full py-8 px-4">
+      <div className="mx-auto max-w-5xl w-full flex justify-between items-center">
+        <Link to="/">
+          <img src={Logo} alt="Logo Web-Motors" />
+        </Link>
+
+        {signed && !loading && (
+          <Link to="/dashboard" className='hover:animate-pulse'>
+            <CircleUserRound size={36} />
+          </Link>
+        )}
+
+        {!signed && !loading && (
+          <Link to="/dashboard"  className='hover:animate-pulse'>
+            <LogInIcon size={36} />
+          </Link>
+        )}
+      </div>
+    </header>
+  );
+};
